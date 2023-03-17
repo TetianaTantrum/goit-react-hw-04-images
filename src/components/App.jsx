c841d35377064b777df88b41ecd33a96c2a9b8de
TetianaTantrum <tetiana.tantrum@gmail.com>
2023-03-17 15:53:33 +0200

    completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -36,7 +36,9 @@ export default function App() {
         handleLoadMore={newImages => handleLoadMore(newImages)}
         setImages={setImages}
       />
-      {images?.length > 0 && <Button onClick={onLoadMore} />}
+      {images && images.length && images.length > 0 && (
+        <Button onClick={onLoadMore} />
+      )}
     </AppStyle>
   );
 }
